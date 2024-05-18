de2e2402562251c759bed6771421467c1eb0b387
nazi <nazipaeez777@gmail.com>
2024-05-18 21:25:57 +0330

    Conver en name month to fa in areaChart

--- src/components/AreachartBox.tsx
+++ src/components/AreachartBox.tsx
@@ -45,8 +45,49 @@ export default function AreachartBox() {
         totalCost: item2 ? item2.totalAmount : 0,
       };
     });
+ 
+    const monthMapping: any = {
+      January: "دی",
+      February: "بهمن",
+      March: "اسفند",
+      April: "فروردین",
+      May: "اردیبهشت",
+      June: "خرداد",
+      July: "تیر",
+      August: "مرداد",
+      September: "شهریور",
+      October: "مهر",
+      November: "آبان",
+      December: "آذر",
+    };
+
+    const convertMonth = result.map((item: any) => {
+      const persianMonth = monthMapping[item.month];
+      return { ...item, month: persianMonth };
+    });
+
+    const monthOrder: any = {
+      فروردین: 1,
+      اردیبهشت: 2,
+      خرداد: 3,
+      تیر: 4,
+      مرداد: 5,
+      شهریور: 6,
+      مهر: 7,
+      آبان: 8,
+      آذر: 9,
+      دی: 10,
+      بهمن: 11,
+      اسفند: 12,
+    };
+
+    const sortedData = convertMonth.sort((a: any, b: any) => {
+      return monthOrder[a.month] - monthOrder[b.month];
+    });
     
-    setMainData(result)
+    setMainData(sortedData)
+
+    console.log(mainData)
   };
 
 
@@ -66,11 +107,11 @@ export default function AreachartBox() {
       <ul className="flex gap-x-4 text-xs 2xs:gap-x-8 sm:text-sm dark:text-zinc-200">
         <li className="flex items-center gap-x-2.5 pt-3">
           <span className="h-4 w-4 bg-[#F84AD0]"></span>
-          <p className="">هزینه</p>
+          <p className="">درآمد</p>
         </li>
         <li className="flex items-center gap-x-2.5 pt-3">
           <span className="h-4 w-4 bg-[#f5d0fe]"></span>
-          <p className="">درامد</p>
+          <p className="">هزینه</p>
         </li>
       </ul>
       <ResponsiveContainer width="112%" height={400}>
